82b5380be4b5c1fd3fccc6cc89dda0afce67c66c
Dvern <dvern@qq.com>
2017-12-22 21:51:30 +0800

    empty add

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -38,5 +38,18 @@ class Index extends BasicController {
         ]);
     }
 
-    
+    public function _empty($name) {
+        $auth = new \thinkcms\auth\Auth();
+        $auth = $auth->autoload($name);
+        if ($auth) {
+            if (isset($auth['code'])) {
+                return json($auth);
+            } elseif (isset($auth['file'])) { 
+                return $auth['file'];
+            }
+            $this->view->engine->layout(false);
+            return $this->fetch($auth[0], $auth[1]);
+        }
+        return abort(404, '页面不存在');
+    }
 }
