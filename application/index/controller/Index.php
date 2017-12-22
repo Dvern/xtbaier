cb748be78559740a07531732db89341250a79f7f
Dvern <dvern@qq.com>
2017-12-22 21:48:33 +0800

    update

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -38,49 +38,5 @@ class Index extends BasicController {
         ]);
     }
 
-    //增加
-    public function add()
-    {
-        //add_post 数据处理
-        if($this->request->isPost()){
-            $post = $this->request->post();
-
-            //数据验证
-            $result = $this->validate($post,$this->validate);
-            if (true !== $result) {
-                return $this->error($result);
-            }
-
-            //写入数据库
-            $post['admin_password'] = Tool::get('helper')->getMd5($post['admin_password']);
-            $add = AdminModel::create($post);
-            if($add){
-
-                //加入角色
-                $authRoleUser = new AuthRoleUser();
-                $authRoleUser->authRoleUserAdd($post['role'], $add['admin_id']);
-
-                return $this->success(lang('Add success'), url($this->url));
-            }else{
-                return $this->error(lang('Add failed'));
-            }
-        }
-
-        //页面渲染
-        $info['role_html'] = self::role();
-
-        return $this->fetch('',[
-            'info' => $info
-        ]);
-    }
-	
-	public function GetAllUserInfo() {
-        $userinfo = new Userinfo();
-        echo $userinfo->GetAllUserInfo();
-    }
-	
-	public function consumeajax() {
-        $tools = new WechatTools();
-        echo $tools->ConsumeCard();
-    }
+    
 }
