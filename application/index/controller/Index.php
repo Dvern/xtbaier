09068ffeca495e2f38cec703d694798f301111ae
Dvern <dvern@qq.com>
2017-12-22 21:41:39 +0800

    no empty

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -27,18 +27,5 @@ class Index extends BasicController {
         $this->assign('navTabs',  parent::navTabs($nav));
     }
 
-    public function _empty($name) {
-        $auth = new \thinkcms\auth\Auth();
-        $auth = $auth->autoload($name);
-        if ($auth) {
-            if (isset($auth['code'])) {
-                return json($auth);
-            } elseif (isset($auth['file'])) { 
-                return $auth['file'];
-            }
-            $this->view->engine->layout(false);
-            return $this->fetch($auth[0], $auth[1]);
-        }
-        return abort(404, '页面不存在');
-    }
+    
 }
